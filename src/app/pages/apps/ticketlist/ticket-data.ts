40fe7c396a5436f271a111f21e3f5467cc5e7280
DanahAlAbdallah <danaalabdallah02@gmail.com>
2024-06-05 16:10:46 +0200

    ticket view

---
+++ src/app/pages/apps/ticketlist/ticket-data.ts
@@ -0,0 +1,14 @@
+import { TicketList } from "./ticket";
+
+
+export const ticketLists: TicketList[] = [
+  new TicketList(
+    1,
+    'new',
+    'Hello',
+    'Hello',
+    'Hello',
+    'Hello',
+    'Hello'
+  )
+];
--- src/app/pages/apps/ticketlist/ticket.ts
+++ src/app/pages/apps/ticketlist/ticket.ts
@@ -1,9 +1,11 @@
-export interface Ticket {
-  id: number;
-  title: string;
-  subtext: string;
-  assignee: string;
-  imgSrc: string;
-  status: string;
-  date: string;
+export class TicketList {
+  constructor(
+    public id: number = 0,
+    public title: string = '',
+    public subtext: string = '',
+    public assignee: string = '',
+    public imgSrc: string = '',
+    public status: string = '',
+    public date: string = ''
+  ) {}
 }
