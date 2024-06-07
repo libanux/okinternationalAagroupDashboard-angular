b8c3eb51aa899a206b96ba936a4098ce2b5fbb4c
nancy-chaalan <150782100+nancy-chaalan@users.noreply.github.com>
2024-06-07 10:38:55 +0300

    edited table css and button and edit actiopn

--- src/app/pages/apps/visa-component/visa-component.component.ts
+++ src/app/pages/apps/visa-component/visa-component.component.ts
@@ -16,9 +16,19 @@ import { VisaClass } from './visaClass';
 })
 
 export class VisaComponentComponent implements OnInit {
-  @ViewChild(MatTable, { static: true }) table: MatTable<any> =
-    Object.create(null);
+
+  @ViewChild(MatTable, { static: true }) table: MatTable<any> = Object.create(null);
   @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator = Object.create(null);
+
+  Name = 'Name';
+  Destination = 'Destination';
+  Duration = 'Duration';
+  Hotels = 'Hotels';
+  Date = 'Date';
+  Seats = 'Seats';
+  Note= 'Note';
+  Status = 'Status';
+
   searchText: any;
   totalCount = -1;
   Cancelled = -1;
@@ -63,82 +73,27 @@ export class VisaComponentComponent implements OnInit {
     return this.dataSource.filteredData.length;
   }
 
-  openDialog(action: string, obj: any): void {
-    // obj.action = action;
-    // const dialogRef = this.dialog.open(AppTicketDialogContentComponent, {
-    //   data: obj,
-    // });
-
-    // dialogRef.afterClosed().subscribe((result) => {
-    //   if (result.event === 'Add') {
-    //     this.addRowData(result.data);
-    //   } else if (result.event === 'Update') {
-    //     this.updateRowData(result.data);
-    //   } else if (result.event === 'Delete') {
-    //     this.deleteRowData(result.data);
-    //   }
-    // });
+  Delete(action: string, obj: any): void {
+  
   }
-  // tslint:disable-next-line - Disables all
-  // addRowData(row_obj: TicketElement): void {
-  //   const d = new Date();
-  //   this.dataSource.data.unshift({
-  //     id: d.getTime(),
-  //     title: row_obj.title,
-  //     subtext: row_obj.subtext,
-  //     assignee: row_obj.assignee,
-  //     status: row_obj.status,
-  //     date: row_obj.date,
-  //   });
-  //   this.table.renderRows();
-  // }
 
-  // tslint:disable-next-line - Disables all
-  // updateRowData(row_obj: TicketElement): boolean | any {
-  //   this.dataSource.data = this.dataSource.data.filter((value, key) => {
-  //     if (value.id === row_obj.id) {
-  //       value.title = row_obj.title;
-  //       value.subtext = row_obj.subtext;
-  //       value.assignee = row_obj.assignee;
-  //       value.status = row_obj.status;
-  //       value.date = row_obj.date;
-  //     }
-  //     return true;
-  //   });
-  // }
+  Update(action: string, obj: any): void {
+  this.Name = obj.name
+  this.Destination = obj.destination
+  this.Duration = obj.duration
+  this.Hotels = obj.hotels
+  this.Date = obj.date
+  this.Seats = obj.nbOfSeats
+  this.Note = obj.note
+  this.Status = obj.status
 
+  }
+ 
   // tslint:disable-next-line - Disables all
   deleteRowData(row_obj: VisaClass): boolean | any {
     this.dataSource.data = this.dataSource.data.filter((value, key) => {
       return value.id !== row_obj.id;
     });
   }
+
 }
-
-// @Component({
-//   // tslint:disable-next-line - Disables all
-//   selector: 'app-dialog-content',
-//   templateUrl: '.././ticket-dialog-content.html',
-// })
-// // tslint:disable-next-line - Disables all
-// export class AppTicketDialogContentComponent {
-//   action: string;
-//   // tslint:disable-next-line - Disables all
-//   local_data: any;
-
-//   constructor(
-//     public dialogRef: MatDialogRef<AppTicketDialogContentComponent>,
-//     @Optional() @Inject(MAT_DIALOG_DATA) public data: TicketElement
-//   ) {
-//     this.local_data = { ...data };
-//     this.action = this.local_data.action;
-//   }
-
-//   doAction(): void {
-//     this.dialogRef.close({ event: this.action, data: this.local_data });
-//   }
-
-//   closeDialog(): void {
-//     this.dialogRef.close({ event: 'Cancel' });
-//   }
-// }
