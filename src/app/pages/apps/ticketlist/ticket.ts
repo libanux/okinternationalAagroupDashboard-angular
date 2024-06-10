9db1791ef915498baaa5d421d6da7947761ee3f8
DanahAlAbdallah <danaalabdallah02@gmail.com>
2024-06-10 13:04:50 +0200

    new

--- src/app/pages/apps/ticketlist/ticket.ts
+++ src/app/pages/apps/ticketlist/ticket.ts
@@ -2,14 +2,13 @@ export class TicketList {
   constructor(
     public _id: number = 0,
     public name: string = '',
-    // public source: string ='',
+    public source: string ='',
     public destination: string = '',
     public duration: string = '',
     public hotels: string = '',
-    // public date: string = '',
     public numberOfPeople: number = 0,
-    public price: number = 0,
-    // public sell: string = '',
+    public price: number = 0, //this is cost
+    public sell: string = '',
     public netprofit: string = '',
     public note: string = '',
     public status: string = '',
