cbb479e86795ac9e1f7a7b473f7f3ce00c804c28
nancy-chaalan <150782100+nancy-chaalan@users.noreply.github.com>
2024-06-13 14:44:31 +0300

    finished first part of stock which is adjust stock

--- src/app/pages/apps/stock/adjust/adjust.component.ts
+++ src/app/pages/apps/stock/adjust/adjust.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Product, products } from 'src/app/classes/products.class';
 
 @Component({
   selector: 'app-adjust',
@@ -7,4 +8,64 @@ import { Component } from '@angular/core';
 })
 export class AdjustComponent {
 
+  ADJUST_ARRAY: Product [] = [];
+  PRODUCTS_ARRAY = products;
+  filteredProducts: any[]
+
+    constructor() {
+      // this.filteredProducts = this.PRODUCTS_ARRAY
 }
+
+    hide = true;
+    hide2 = true;
+    conhide = true;
+    alignhide = true;
+  
+
+    // 3 accordian
+    step = 0;
+  
+    // setStep(index: number) { this.step = index; }
+  
+    // nextStep() { this.step++; }
+  
+    // prevStep() { this.step--; }
+  
+    panelOpenState = false;
+
+    displayedColumns: string[] = [  
+    'barcode',
+    'itemName',
+    'category',
+    'previousQuantity',
+    'onHandQuantity',
+    'action'
+      ];
+    
+      searchQuery: string;
+    editRowIndex: number = -1;
+
+    onEdit(element: Element, field: string, event: any, rowIndex: number) {
+      this.editRowIndex = rowIndex; // Set the rowIndex to highlight the editing cell
+      // Handle your edit logic here
+    }
+
+
+
+    filterProducts() {
+      // this.filteredProducts = this.dataSource
+      const query = this.searchQuery.toLowerCase();
+      // this.filteredProducts = this.dataSource.filter(product => product.itemName.toLowerCase().includes(query));
+    }
+  
+    addNewProduct() {
+      // Logic to add a new product
+      console.log('Add new product clicked');
+    }
+  
+    displayFn(product: { id: number, name: string }): string {
+      return product ? product.name : '';
+    }
+  }
+  
+
